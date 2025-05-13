e0dc39f4dd54edb2062cd9daf3709c706470bc28
tnm <t@ted.io>
2025-05-13 09:37:06 -0700

    0.2.2

--- src/kit/__init__.py
+++ src/kit/__init__.py
@@ -3,7 +3,7 @@ A modular toolkit for LLM-powered codebase understanding.
 """
 
 __author__ = "cased"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .code_searcher import CodeSearcher
 from .context_extractor import ContextExtractor
